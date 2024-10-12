22d1ad0baa3d6a9dd694d91517fa58d88e4b3143
Adam Pierzynowski <kontakt@devdraw.pl>
2024-10-12 21:15:54 +0200

    HTML, CSS and JS for contact form

---
+++ js/kontakt.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('message_form')
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.error('Brak obsługi wysyłki')
+})
--- pages/kontakt.php
+++ pages/kontakt.php
@@ -2,7 +2,17 @@
 <meta name="description" content="Find out who we are and what we do on our about page.">
 <meta name="keywords" content="about us, company, team">
 <div class="content">
-    <h1>
-        Contact
-    </h1>
+<h1>Contact us</h1>
+    <form action="" method="POST" id="message_form">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email">
+
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" rows="5" cols="30"></textarea>
+
+        <button type="submit">Wyślij</button>
+    </form>
 </div>

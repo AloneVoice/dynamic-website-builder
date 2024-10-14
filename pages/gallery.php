c9fd414ab1201254821fd9c5a0656a6806dd62f2
Adam Pierzynowski <kontakt@devdraw.pl>
2024-10-14 18:18:09 +0200

    Add home page video background and change colors

--- pages/gallery.php
+++ pages/gallery.php
@@ -3,8 +3,5 @@
 <meta name="description" content="Welcome to our site!">
 <meta name="keywords" content="home, main page">
 <div class="content">
-<video autoplay="" muted="" loop="" playsinline="">
-    <source src="./assets/back.mp4" type="video/mp4">
-    Your browser does not support the video tag.
-</video>
+
 </div>
--- pages/oferta.php
+++
@@ -1,9 +0,0 @@
-<title>Oferta</title>
-<meta name="description" content="Tu znajdziesz ofertę">
-<meta name="keywords" content="oferta, produkty">
-<div class="content">
-    <h1>
-        Offer
-    </h1>
-    <p>Here is out offer...</p>
-</div>
